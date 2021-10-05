433f8bdbddf6e5de5068f361355aa8fe5f480f5e
Daniel Burgos <dfburgosm@correo.udistrital.edu.co>
2021-10-05 09:30:48 -0500

    Se reidenta el formato del archivo productosMapper para mejorar la legibilidad

--- src/main/java/com/dmaktech/tienda/productos/ProductoMapper.java
+++ src/main/java/com/dmaktech/tienda/productos/ProductoMapper.java
@@ -15,16 +15,28 @@ public class ProductoMapper {
 
     public static Collection<Producto> csvToProducts(MultipartFile file) {
 
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream(), "UTF-8"));
-                CSVParser productosCSV = CSVFormat.Builder.create(CSVFormat.DEFAULT)
-                        .setHeader("codigo", "nombre", "nitProveedor", "precioCompra", "iva", "precioVenta").build()
-                        .parse(reader)) {                            
-            return productosCSV.stream().map(registro -> {
-                return new Producto(Long.parseLong(registro.get("codigo")), registro.get("nombre"),
-                        Double.parseDouble(registro.get("iva")), Long.parseLong(registro.get("nitProveedor")),
+        try (
+                BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream(), "UTF-8"));
+                CSVParser productosCSV = CSVFormat
+                    .Builder
+                    .create(CSVFormat.DEFAULT)
+                    .setHeader("codigo", "nombre", "nitProveedor", "precioCompra", "iva", "precioVenta")
+                    .build()
+                    .parse(reader)
+            ) 
+        {                            
+            return productosCSV
+                .stream()
+                .map(registro -> {
+                    return new Producto(
+                        Long.parseLong(registro.get("codigo")),
+                        registro.get("nombre"),
+                        Double.parseDouble(registro.get("iva")),
+                        Long.parseLong(registro.get("nitProveedor")),
                         Double.parseDouble(registro.get("precioCompra")),
                         Double.parseDouble(registro.get("precioVenta")));
-            }).collect(Collectors.toList());
+                })
+                .collect(Collectors.toList());
 
         } catch (Exception e) {            
             throw new CSVFileException("No se pudo procesar el archivo CSV: " + e.getMessage());

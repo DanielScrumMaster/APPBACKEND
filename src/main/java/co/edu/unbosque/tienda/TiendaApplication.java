ad9b48326b4f0f08c9fa6aaabf99afc3d73683c5
Daniel Burgos <dfburgosm@correo.udistrital.edu.co>
2021-10-20 21:30:30 -0500

    Se agrega el soporte de CORS

--- src/main/java/co/edu/unbosque/tienda/TiendaApplication.java
+++ src/main/java/co/edu/unbosque/tienda/TiendaApplication.java
@@ -3,12 +3,25 @@ package co.edu.unbosque.tienda;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class TiendaApplication /*extends SpringBootServletInitializer*/ {
+public class TiendaApplication /* extends SpringBootServletInitializer */ {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TiendaApplication.class, args);
 	}
-	
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:8080")
+					.allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
+	}
 }
